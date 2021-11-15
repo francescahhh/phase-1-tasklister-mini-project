3cbe85c34e4dad6c2963662bb84424fb49fe7688
Francesca Hamm <francesca.c.hamm@gmail.com>
2021-11-14 17:59:45 -0700

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
+//code from first try:
+// document.addEventListener("DOMContentLoaded", () => {
+
+//   document.getElementById("create-task-form").addEventListener("submit", function(event) {
+//     //create child node
+//     let taskList = document.getElementById("tasks");
+//     let task = document.createElement("li");
+//     let taskInput = document.getElementById("new-task-description").value;
+//     let taskNode = document.createTextNode(taskInput);
+
+//     //append child node to list
+//     task.appendChild(taskNode);
+//     taskList.appendChild(task);
+
+//     event.preventDefault();
+//   });
+// });
+
+//marc's review solution:
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const todoForm = document.getElementById('create-task-form');
+  todoForm.addEventListener('submit', handleSubmit);
 });
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  //setting up the task list
+  const taskList = document.getElementById('tasks');
+  const task = document.createElement('li');
+  task.textContent = (event.target.description.value);
+  taskList.append(task);
+
+  //creating delete button
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'X';
+  deleteButton.addEventListener('click', () => {
+    taskList.removeChild(task);
+    //or task.remove();
+});
+  task.append(deleteButton);
+
+
+  //to clear out the text box after submitted
+  event.target.reset();
+}
